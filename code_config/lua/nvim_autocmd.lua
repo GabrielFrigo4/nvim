5b159bcee5995eb135fb882e9a1ddc1d854553f4
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-07-17 15:24:15 -0300

    Update nvim_autocmd.lua

--- code_config/lua/nvim_autocmd.lua
+++ code_config/lua/nvim_autocmd.lua
@@ -1,6 +1,6 @@
 -- Config file extension
 local function setExtCmd(extension, command)
-    Nvim.autocmd({'BufNewFile', 'BufRead'} ,{
+    Nvim.autocmd({ 'BufNewFile', 'BufRead' }, {
         pattern = extension,
         command = command
     })
@@ -8,34 +8,40 @@ end
 
 local treesitter = require('nvim-treesitter.configs')
 treesitter.setup({
-	ensure_installed = {"c", "cpp", "cs", "lua", 
-        "rust", "java", "vim", "python", "bash"},
-	highlight = { enable = true },
-	indent = { enable = true },
+    ensure_installed = {
+        "c", "cpp", "c_sharp", "lua", "rust", "java", "python", "zig", "javascript", -- languages
+        "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",       -- github
+        "vim", "bash", "arduino",
+        "cuda", "glsl", "hlsl",
+        "yaml", "toml",
+        "make", "cmake",
+    },
+    highlight = { enable = true },
+    indent = { enable = true },
 
-	-- setup modules
-	matchup = { enable = true },
+    -- setup modules
+    matchup = { enable = true },
 })
 
 -- MASM
-setExtCmd({'*.masm', '*.minc'}, 'set ft=masm')
+setExtCmd({ '*.masm', '*.minc' }, 'set ft=masm')
 -- NASM
-setExtCmd({'*.nasm', '*.ninc'}, 'set ft=nasm')
+setExtCmd({ '*.nasm', '*.ninc' }, 'set ft=nasm')
 -- ARM
-setExtCmd({'*.arm', '*.ainc'}, 'set ft=arm')
+setExtCmd({ '*.arm', '*.ainc' }, 'set ft=arm')
 -- GAS
-setExtCmd({'*.s', '*.sinc'}, 'set ft=asm')
+setExtCmd({ '*.s', '*.sinc' }, 'set ft=asm')
 -- RISC-V
-setExtCmd({'*.riscv', '*.rinc'}, 'set ft=riscv')
+setExtCmd({ '*.riscv', '*.rinc' }, 'set ft=riscv')
 
 -- Windows
 if Nvim.isWin then
     -- ASM
-    setExtCmd({'*.asm', '*.inc'}, 'set ft=masm')
+    setExtCmd({ '*.asm', '*.inc' }, 'set ft=masm')
 end
 
 -- Linux
 if Nvim.isUnix then
     -- ASM
-    setExtCmd({'*.asm', '*.inc'}, 'set ft=nasm')
+    setExtCmd({ '*.asm', '*.inc' }, 'set ft=nasm')
 end

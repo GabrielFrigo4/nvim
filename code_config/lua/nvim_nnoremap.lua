2a31b5cedad063c90b68c28573f6332e65b7eaf3
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-04-12 19:09:49 -0300

    Update

--- code_config/lua/nvim_nnoremap.lua
+++ code_config/lua/nvim_nnoremap.lua
@@ -5,15 +5,9 @@ local function nrmap(shortcut, command)
     Nvim.keymap.set('', shortcut, command, { noremap = true, silent = true })
 end
 
--- Only work on Windows
---nrmap('<C-.>', '<C-]>')
---nrmap('<C-,>', '<C-o>')
-
 -- Work on Windows and Linux
 nrmap('<M-.>', '<C-]>')
 nrmap('<M-,>', '<C-o>')
---nrmap('<leader>.', '<C-]>')
---nrmap('<leader>,', '<C-o>')
 nrmap('<leader>pi', ':PlugInstall<cr>')
 nrmap('<leader>pu', ':PlugUpdate<cr>')
 nrmap('<leader>urp', ':UpdateRemotePlugins<cr>')
--- ginit.vim
+++ ginit.vim
@@ -1,1 +1,2 @@
 GuiFont RobotoMono Nerd Font:h11:b
+set guicursor+=a:-blinkwait600-blinkoff550-blinkon600

8d30b096b316f4c0b38153ed3ba89a4f52c42287
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-04-16 18:21:29 -0300

    Update nvim_nnoremap.lua

--- code_config/lua/nvim_nnoremap.lua
+++ code_config/lua/nvim_nnoremap.lua
@@ -5,10 +5,21 @@ local function nrmap(shortcut, command)
     Nvim.keymap.set('', shortcut, command, { noremap = true, silent = true })
 end
 
--- Work on Windows and Linux
+-- Link Navigate Shortcut
 nrmap('<M-.>', '<C-]>')
 nrmap('<M-,>', '<C-o>')
+
+-- Window Navigate Shortcut
+nrmap('<S-tab>', '<C-w>w')
+nrmap('<M-up>', '<C-w><up>')
+nrmap('<M-down>', '<C-w><down>')
+nrmap('<M-left>', '<C-w><left>')
+nrmap('<M-right>', '<C-w><right>')
+
+-- Plug Shortcut
 nrmap('<leader>pi', ':PlugInstall<cr>')
 nrmap('<leader>pu', ':PlugUpdate<cr>')
 nrmap('<leader>urp', ':UpdateRemotePlugins<cr>')
+
+-- NvimTree Shortcut
 nrmap('<C-\\>', ':NvimTreeToggle<cr>')

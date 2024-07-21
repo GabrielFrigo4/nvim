1c3c16e48487b7859430fbf21e7590d6e7906cea
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-07-21 10:53:29 -0300

    Update nvim_plug.lua

--- code_config/lua/nvim_plug.lua
+++ code_config/lua/nvim_plug.lua
@@ -14,7 +14,7 @@ Nvim.plug.get('nvim-tree/nvim-tree.lua')
 Nvim.plug.get('stevearc/oil.nvim')
 
 -- bottom plug
-Nvim.plug.get('terryma/vim-multiple-cursors')
+Nvim.plug.get('mg979/vim-visual-multi')
 Nvim.plug.get('sheerun/vim-polyglot')
 Nvim.plug.get('ARM9/arm-syntax-vim')
 Nvim.plug.get('kylelaker/riscv.vim')
